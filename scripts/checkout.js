a8421f36ca6131228f301bfd0acee7a5f010e9b4
Luck <kennethcorpuz88@gmail.com>
2024-07-19 22:47:58 +0800

    Delete product from html when clicking delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
     console.log(matchingProducts);
 
     
-    cartSummaryHTML += `<div class="cart-item-container">
+    cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${matchingProducts.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -103,7 +103,10 @@ document.querySelectorAll('.js-delete-links').forEach((link) => {
     const productId = link.dataset.productId;
 
     removeFromCart(productId);
-    console.log(cart);
+    
+    
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+    container.remove();
   });
 });
-
